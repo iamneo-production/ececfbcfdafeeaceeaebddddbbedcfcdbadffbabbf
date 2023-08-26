3e222ec2973950d45b8d638bb3a275f2a5d9dce2
Iamneo <techadmin@iamneo.ai>
2023-08-26 09:25:59 +0000

    Backup Commit

---
+++ reactapp/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Stopwatch from './components/Stopwatch/Stopwatch';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Stopwatch/>
+      
+    </div>
+    
+
+  );
+}
+
+export default App;
